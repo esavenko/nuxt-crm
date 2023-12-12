dd84e6c8833175338e92268e74509ecf2fd43ed1
raredead <smahelzed@gmail.com>
2023-12-12 17:29:52 +0300

    Подключил Tailwind, Shadcn и настроил конфиги

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+    <aside class="px-5 py-8 bg-sidebar h-full revative w-full">
+        <NuxtLink to="/" class="mb-10 block">
+            <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
+        </NuxtLink>
+
+        <UiButton>Send</UiButton>
+    </aside>
+</template>
