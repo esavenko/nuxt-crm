b58bbb81c5c452b91f14dbae95d37916c999eada
raredead <smahelzed@gmail.com>
2023-12-12 15:05:51 +0300

    Инициализировал проект

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="section-layout" style="min-height: 100vh;">
+        <aside>Sidebar</aside>
+        <slot />
+    </section>
+</template>
+
+<style scoped>
+.section-layout {
+    display: grid;
+    grid-template-columns: 1fr 6fr;
+}
+</style>
