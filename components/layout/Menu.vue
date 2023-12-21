7ea56f40ea979d10ee0003164c0592e213c0c520
raredead <smahelzed@gmail.com>
2023-12-21 14:13:23 +0300

    Компонент меню и дата

---
+++ components/layout/Menu.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { MENU_DATA } from './menu.data';
+</script>
+
+<template>
+    <div>
+        <NuxtLink class="flex items-center py-1 px-3 rounded-lg w-full hover:bg-gray-700 hover:shadow transition-all mb-2"
+            v-for="item in MENU_DATA" :key="item.name" :to="item.url">
+            <Icon :name="item.icon" class="mr-3" />
+            <span>{{ item.name }}</span>
+        </NuxtLink>
+    </div>
+</template>
---
+++ components/layout/menu.data.ts
@@ -0,0 +1,43 @@
+export interface IMenuItem {
+  name: string
+  url: string
+  icon: string
+}
+
+export const MENU_DATA: IMenuItem[] = [
+  {
+    icon: 'radix-icons:dashboard',
+    name: 'Home',
+    url: '/'
+  },
+  {
+    icon: 'ep:goods',
+    name: 'Products',
+    url: '/products'
+  },
+  {
+    icon: 'ph:contactless-payment',
+    name: 'Payments',
+    url: '/payments'
+  },
+  {
+    icon: 'fluent:receipt-28-regular',
+    name: 'Orders',
+    url: '/orders'
+  },
+  {
+    icon: 'fluent:person-feedback-48-regular',
+    name: 'Feedback',
+    url: '/feedback'
+  },
+  {
+    icon: 'radix-icons:gear',
+    name: 'Settings',
+    url: '/settings'
+  },
+  {
+    icon: 'radix-icons:question-mark',
+    name: 'Help Center',
+    url: '/help'
+  }
+]
