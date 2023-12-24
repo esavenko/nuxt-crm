3b4b735ad1fc26c29ce879a7a300f864a50dbfa2
raredead <smahelzed@gmail.com>
2023-12-24 15:34:38 +0300

    Обновление конфигов проекта

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,22 +4,26 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/image',
     '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    'nuxt-icon',
-    [
+    'shadcn-nuxt'
+      [
       '@nuxtjs/google-fonts',
-      {
-        families: {
-          Lato: {
-            wght: [300, 400, 500],
-            ital: [300]
+        {
+          families: {
+            Lato: {
+              wght: [300, 400, 500],
+              ital: [300]
+            }
           }
         }
-      }
-    ]
+      ],
+    'nuxt-icon',
+    '@pinia/nuxt'
   ],
   shadcn: {
     prefix: 'Ui',
     componentDir: './components/ui'
+  },
+  pinia: {
+    storesDirs: ['./store/**']
   }
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,97 +1,97 @@
-const animate = require("tailwindcss-animate")
+const animate = require('tailwindcss-animate')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  
+  darkMode: ['class'],
+
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
-      },
+        '2xl': '1400px'
+      }
     },
     fontSize: {
-      xs: '0.9rem',
-      sm: '1.07rem',
-      base: '1.18rem',
-      lg: '1.24rem',
-      xl: '1.38rem',
+      xs: '1.17rem',
+      sm: '1.39rem',
+      base: '1.53rem',
+      lg: '1.61rem',
+      xl: '1.79rem'
     },
     extend: {
       transitionDuration: {
-        DEFAULT: '333ms',
+        DEFAULT: '333ms'
       },
       transitionTimingFunction: {
-        DEFAULT: 'ease-in-out',
+        DEFAULT: 'ease-in-out'
       },
       colors: {
-        border: "var(--border)",
-        input: "var(--input)",
-        ring: "var(--ring)",
-        background: "var(--background)",
-        foreground: "hsl(var(--foreground))",
-        sidebar: "var(--sidebar)",
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'hsl(var(--foreground))',
+        sidebar: 'var(--sidebar)',
         primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "var(--primary-foreground)",
+          DEFAULT: 'var(--primary)',
+          foreground: 'var(--primary-foreground)'
         },
         secondary: {
-          DEFAULT: "var(--secondary)",
-          foreground: "hsl(var(--secondary-foreground))",
+          DEFAULT: 'var(--secondary)',
+          foreground: 'hsl(var(--secondary-foreground))'
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
         muted: {
-          DEFAULT: "var(--muted)",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'var(--muted)',
+          foreground: 'hsl(var(--muted-foreground))'
         },
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         card: {
-          DEFAULT: "var(--card)",
-          foreground: "hsl(var(--card-foreground))",
-        },
+          DEFAULT: 'var(--card)',
+          foreground: 'hsl(var(--card-foreground))'
+        }
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       keyframes: {
-        "accordion-down": {
+        'accordion-down': {
           from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 }
         },
-        "collapsible-down": {
+        'collapsible-down': {
           from: { height: 0 },
-          to: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 'var(--radix-collapsible-content-height)' }
         },
-        "collapsible-up": {
+        'collapsible-up': {
           from: { height: 'var(--radix-collapsible-content-height)' },
-          to: { height: 0 },
-        },
+          to: { height: 0 }
+        }
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-in-out",
-        "collapsible-up": "collapsible-up 0.2s ease-in-out",
-      },
-    },
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+        'collapsible-down': 'collapsible-down 0.2s ease-in-out',
+        'collapsible-up': 'collapsible-up 0.2s ease-in-out'
+      }
+    }
   },
-  plugins: [animate],
+  plugins: [animate]
 }
