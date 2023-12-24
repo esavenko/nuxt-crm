cf340b3911e7e55624613d4d1834101f479967cc
raredead <smahelzed@gmail.com>
2023-12-24 15:35:32 +0300

    ОДоделана страница логина

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { v4 as uuid } from 'uuid'
+
 useHead({
   title: 'Login | CRM system'
 })
@@ -6,6 +8,35 @@ useHead({
 const email = ref('')
 const password = ref('')
 const name = ref('')
+
+const isLoadingStore = useIsLoadingStore()
+const authStore = useAuthStore()
+const router = useRouter()
+
+const login = async () => {
+  isLoadingStore.set(true)
+  await account.createEmailSession(email.value, password.value)
+  const response = await account.get()
+  if (response) {
+    authStore.set({
+      email: response.email,
+      name: response.name,
+      status: response.status
+    })
+  }
+
+  email.value = ''
+  password.value = ''
+  name.value = ''
+
+  await router.push('/')
+  isLoadingStore.set(false)
+}
+
+const register = async () => {
+  await account.create(uuid(), email.value, password.value, name.value)
+  await login()
+}
 </script>
 
 <template>
@@ -33,8 +64,8 @@ const name = ref('')
           class="mb-3"
         />
         <div class="flex items-center justify-center gap-5">
-          <UiButton type='button'>Login</UiButton>
-          <UiButton type='button'>Register</UiButton>
+          <UiButton type='button' @click="login">Login</UiButton>
+          <UiButton type='button' @click="register">Register</UiButton>
         </div>
       </form>
     </div>
