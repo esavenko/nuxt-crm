0ab2796d962ab181ed51ffc0dda9d40d5f472131
raredead <smahelzed@gmail.com>
2023-12-24 15:36:08 +0300

    ДоÐбавлен функционал по выходу из аккаунта

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,11 +1,29 @@
+<script setup lang="ts">
+import { useAuthStore, useIsLoadingStore } from '~/store/auth.store'
+
+const store = useAuthStore()
+const isLoadingStore = useIsLoadingStore()
+const router = useRouter()
+
+const logout = async () => {
+  isLoadingStore.set(true)
+  await account.deleteSession('current')
+  store.clear()
+  await router.push('/login')
+  isLoadingStore.set(false)
+}
+</script>
+
 <template>
-    <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
-        <NuxtLink to="/" class="mb-10 block">
-            <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
-        </NuxtLink>
-        <button class="absolute top-5 right-3 transition-colors hover:text-primary">
-            <Icon name="line-md:log-out" size="22" />
-        </button>
-        <LayoutMenu />
-    </aside>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
+    </NuxtLink>
+    <button
+      @click="logout"
+      class="absolute top-5 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:log-out" size="22" />
+    </button>
+    <LayoutMenu />
+  </aside>
 </template>
