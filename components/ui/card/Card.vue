77783e64622ab08fb05ce2203f9d514b4cc0acfa
raredead <smahelzed@gmail.com>
2023-12-25 23:52:10 +0300

    конпонент карточки из shadcn

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { cn } from '@/utils'
+
+const props = defineProps({
+  class: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-lg bg-card text-card-foreground animation border border-transparent transition-colors hover:border-[#a252c83d]',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@keyframes show {
+  from {
+    @apply border-[#a252c83d];
+    transform: scale(0.5) translateY(-30px);
+    opacity: .4;
+  }
+
+  90% {
+    @apply border-[#a252c83d];
+  }
+
+  to {
+    @apply border-transparent;
+    transform: scale(1) translateY(0);
+    opacity: 1;
+  }
+}
+
+.animation {
+  animation: show .3s ease-in-out;
+}
+</style>
