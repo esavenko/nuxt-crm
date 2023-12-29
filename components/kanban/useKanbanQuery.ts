337e9c396123ab6604316dc69dd196e1b1048bb3
raredead <smahelzed@gmail.com>
2023-12-29 16:01:19 +0300

    доделан функционал взаимодействия канбана и сервера

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -10,6 +10,22 @@ export function useKanbanQuery() {
     select(data) {
       const newBoard = [...KANBAN_DATA]
       const deals = data.documents as unknown as IDeal[]
+
+      for (const deal of deals) {
+        const column = newBoard.find(col => col.id === deal.status)
+        if (column) {
+          column.items.push({
+            $createdAt: deal.$createdAt,
+            id: deal.$id,
+            name: deal.name,
+            price: deal.price,
+            companyName: deal.customer.name,
+            status: deal.status
+          })
+        }
+      }
+
+      return newBoard
     }
   })
 }
