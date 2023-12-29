aeebf505d5f933a944f6d1e23443b2c6eea4f3bb
raredead <smahelzed@gmail.com>
2023-12-29 15:56:22 +0300

    компонент создания сделÐки

---
+++ components/kanban/CreateDeal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useMutation } from '@tanstack/vue-query'
+import { v4 as uuid } from 'uuid'
+import { defineProps } from 'vue'
+import { COLLECTION_DEALS, DB_ID } from '~/utils/app.constants'
+import { IDeal } from '~/types/deals.types'
+
+const isOpenForm = ref(false)
+</script>
+
+<template>
+  <div class="text-center mb-2">
+    <button
+      class="transition-all opacity-5 hover:opacity-100 hover:text-[#a252c8]"
+      @click="isOpenForm = !isOpenFrom"
+    >
+      <Icon
+        v-if="isOpenForm"
+        name="radix-icons:arrow-up"
+        class="fade-in-100 fade-out-0"
+        size="25"
+      />
+      <Icon
+        v-if="!isOpenForm"
+        name="radix-icons:plus-circled"
+        class="fade-in-100 fade-out-0"
+        size="25"
+      />
+    </button>
+  </div>
+</template>
