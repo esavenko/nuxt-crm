852206e5b121fd8120196eb28f2195203365e7cc
raredead <smahelzed@gmail.com>
2024-01-02 11:21:40 +0300

    исправлена ошибка перетаскивания сделоÑк

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -2,13 +2,17 @@ import { useQuery } from '@tanstack/vue-query'
 import { DB_ID, COLLECTION_DEALS } from '~/utils/app.constants'
 import { KANBAN_DATA } from '~/components/kanban/kanban.data'
 import type { IDeal } from '~/types/deals.types'
+import type { IColumn } from '~/components/kanban/kanban.types'
 
 export function useKanbanQuery() {
   return useQuery({
     queryKey: ['deals'],
     queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
     select(data) {
-      const newBoard = [...KANBAN_DATA]
+      const newBoard: IColumn[] = KANBAN_DATA.map(column => ({
+        ...column,
+        items: []
+      }))
       const deals = data.documents as unknown as IDeal[]
 
       for (const deal of deals) {
