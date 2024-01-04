8e665fd11c2b28426373d513221009349e7633f5
raredead <smahelzed@gmail.com>
2024-01-04 16:11:35 +0300

    компонент сÐлайдовера и его подкомпоненты

---
+++ components/kanban/slideover/Comments.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useDealSlideStore } from '@/store/deal-slide.store'
+
+const store = useDealSlideStore()
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ components/kanban/slideover/Top.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+import { useDealSlideStore } from '@/store/deal-slide.store'
+import { convertCurrency } from '@/utils/convertCurrency'
+
+const store = useDealSlideStore()
+</script>
+
+<template>
+  <div class="border-border bg-black/20 rounded p-3">
+    <div class="uppercase bold text-xl mb-4">О сделке</div>
+    <KanbanSlideoverLabel label-text="Наименование">
+      <h2>{{ store.card?.name }}</h2>
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Сумма">
+      {{ convertCurrency(store.card?.price) }}
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Статус">
+      <UiBadge variant="outline">
+        {{ store.card?.status }}
+      </UiBadge>
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Клиент">
+      {{ store.card?.companyName }}
+    </KanbanSlideoverLabel>
+    <KanbanSlideoverLabel label-text="Дата создания">
+      {{ dayjs(store.card?.$createdAt).format('DD MMMM YYYY') }}
+    </KanbanSlideoverLabel>
+  </div>
+</template>
